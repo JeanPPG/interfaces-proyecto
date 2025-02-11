dc7ff701cdceb24909d23870f30ffa75e42a259a
ChristianDonoso23 <cedonoso@espe.edu.ec>
2025-02-11 07:56:03 -0500

    FIX Prueba

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import './index.css';
 import App from './App';
 import Login from './components/login';
-import Register from './components/register';
+import Register from './components/Register';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
